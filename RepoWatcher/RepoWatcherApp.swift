ca2e4465dc15683d9c0fda7fb296b96b84ceb14e
Peter Hartnett <peter.hartnett@gmail.com>
2022-08-29 08:13:40 -0400

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Peter Hartnett on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
